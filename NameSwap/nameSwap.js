273b3bb48e17a13bcee7489296297a77fbfb1235
Alex Federovitch <alexfederovitch@yahoo.com>
2021-03-12 14:24:09 -0600

    Update nameSwap.js

--- NameSwap/nameSwap.js
+++ NameSwap/nameSwap.js
@@ -1,1 +1,13 @@
+// Name Swap
+ 
+// Challenge
+  // Reverse the order of two provided strings.
+ 
+// Examples
+  // "Abraham Lincoln" ➞ "Lincoln Abraham"
+  // "Hank Aaron" ➞ "Aaron Hank"
 
+const nameSwap = (name) => {
+  let newName = name.split(" ").reverse().join(" ")
+  return newName
+}
